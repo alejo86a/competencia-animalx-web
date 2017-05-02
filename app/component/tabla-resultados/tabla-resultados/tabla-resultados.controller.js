67113bd0d2f8a6fa0e4845f09c70f5f5f0e019b7
ser0125 <sergio.llanos.garcia@gmail.com>
2017-05-02 00:14:43 -0500

    Vista de animalx

--- app/component/tabla-resultados/tabla-resultados/tabla-resultados.controller.js
+++ app/component/tabla-resultados/tabla-resultados/tabla-resultados.controller.js
@@ -1,13 +1,230 @@
-function tablaResultadosController($stateParams){
-  var ctrl = this;
+function tablaResultadosController($stateParams) {
+    var ctrl = this;
 
     console.log("tabla resultados")
-  ctrl.$onInit = function(){
-    ctrl.saludo="hola mundo: "+$stateParams.nombretorneo;
-
-  }
+    ctrl.$onInit = function() {
+      //  ctrl.saludo = "hola mundo: " + $stateParams.nombretorneo;
+        ctrl.arreglo = [{
+            "baseActual": "5",
+            "bases": [{
+                    "base": "1",
+                    "completada": "si",
+                    "puntos": "20"
+                },
+                {
+                    "base": "2",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "6",
+                    "completada": "si",
+                    "puntos": "40"
+                }
+            ],
+            "participante": "Sergio Andres",
+            "torneo": "torneosergi"
+        }, {
+            "baseActual": "5",
+            "bases": [{
+                    "base": "",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "2",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "6",
+                    "completada": "si",
+                    "puntos": "40"
+                }
+            ],
+            "participante": "Jose Andres",
+            "torneo": "torneosergi"
+        }, {
+            "baseActual": "5",
+            "bases": [{
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "2",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "6",
+                    "completada": "si",
+                    "puntos": "40"
+                }
+            ],
+            "participante": "Martin Elias",
+            "torneo": "torneosergi"
+        }, {
+            "baseActual": "5",
+            "bases": [{
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "2",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "6",
+                    "completada": "si",
+                    "puntos": "40"
+                }
+            ],
+            "participante": "Alex Fernando",
+            "torneo": "torneosergi"
+        }, {
+            "baseActual": "5",
+            "bases": [{
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "60"
+                },
+                {
+                    "base": "2",
+                    "completada": "si",
+                    "puntos": "30"
+                },
+                {
+                    "base": "3",
+                    "completada": "si",
+                    "puntos": "40"
+                },
+                {
+                    "base": "4",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "5",
+                    "completada": "si",
+                    "puntos": "50"
+                },
+                {
+                    "base": "6",
+                    "completada": "si",
+                    "puntos": "40"
+                }
+            ],
+            "participante": "Jaime Isaza",
+            "torneo": "torneosergi"
+        }];
+        ctrl.torneo = {
+            "bases": [{
+                    "juez": "",
+                    "numeroBase": "1"
+                },
+                {
+                    "juez": "",
+                    "numeroBase": "2"
+                },
+                {
+                    "juez": "",
+                    "numeroBase": "3"
+                },
+                {
+                    "juez": "",
+                    "numeroBase": "4"
+                },
+                {
+                    "juez": "",
+                    "numeroBase": "5"
+                },
+                {
+                    "juez": "",
+                    "numeroBase": "6"
+                }
+            ],
+            "juecesDisponibles": [{
+                "nombreJuez": ""
+            }],
+            "nombreTorneo": "Torneo el mejor",
+            "participantesDisponibles": [{
+                "asignado": "",
+                "nombre": ""
+            }]
+        };
+        ctrl.total=function(fila){
+          var total=0;
+          for(var i=0;i<ctrl.arreglo[fila].bases.length;i++){
+            total+=Number(ctrl.arreglo[fila].bases[i].puntos);
+        }
+          return total;
+        }
+    }
 }
 
 angular
-  .module('component.tablaResultados')
-  .controller('tablaResultadosController',tablaResultadosController)
+    .module('component.tablaResultados')
+    .controller('tablaResultadosController', tablaResultadosController);
